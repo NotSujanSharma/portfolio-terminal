a6eccb1fe469569bc75d8f66d481ebf71f02eb7e
NotSujanSharma <ssharma627@myseneca.ca>
2024-03-29 02:17:47 -0400

    removed a bunch of if else

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const [terminalLines, setTerminalLines] = useState([]);
   const inputRef = useRef(null);
-  const terminalBodyRef = useRef(null); 
+  const terminalBodyRef = useRef(null);
 
   useEffect(() => {
     inputRef.current.focus();
@@ -14,71 +14,48 @@ function App() {
     if (terminalBodyRef.current) {
       terminalBodyRef.current.scrollTop = terminalBodyRef.current.scrollHeight;
     }
-  }, [terminalLines]); 
+  }, [terminalLines]);
+
+  const executeCommand = (command) => {
+    const commands = {
+      ls: ["info.txt", "skills.txt", "projects.txt", "contact.txt"],
+      pwd: ["/home/ak"],
+      id: ["uid=1000(ak) gid=1000(ak) groups=1000(ak),4(adm),24(cdrom),27(sudo),126(sambashare)"],
+      whoami: ["ak"],
+      "uname -a": ["Linux ak 5.4.0-80-generic #90-Ubuntu x86_64 GNU/Linux - Portfolio Terminal"],
+      uname: ["Linux"],
+      "cat info.txt": ["Name: Sujan Sharma", "Location: On, Canada", "Occupation: Computer Programmer"],
+      "cat skills.txt": ["Languages: JavaScript, Python, Java, C++, C, Ruby", "Frameworks: Django, React, Node.js, Express, Flask, Ruby on Rails", "Databases: MySQL, PostgreSQL, MongoDB, SQLite", "Tools: Git, Docker, Jenkins, Travis CI, Circle CI, Heroku, AWS, GCP"],
+      "cat projects.txt": ["1. Portfolio Terminal", "2. QBX", "3. Dynamic YouTube title"],
+      "cat contact.txt": ["Email: sujan@brokenai.ca, sujansharma2060@gmail.com", "LinkedIn: https://www.linkedin.com/in/NotSujanSharma/", "GitHub: https://www.github.com/NotSujanSharma"],
+      help: ["Available commands are: ls, cat, pwd, id, whoami, uname, clear, help"],
+    };
+
+    if (command === "clear" || command === "cls") {
+      setTerminalLines([]);
+    } else if (commands[command]) {
+      setTerminalLines((lines) => [
+        ...lines,
+        `┌──(ak㉿ak)-[~]`,
+        `└─$ ${command}`,
+        ...commands[command],
+        ` `
+      ]);
+    } else {
+      setTerminalLines((lines) => [
+        ...lines,
+        `┌──(ak㉿ak)-[~]`,
+        `└─$ ${command}`,
+        `command not found: ${command}`,
+        ` `
+      ]);
+    }
+  };
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
-      const content = event.target.value;
-      let newLines = [...terminalLines];
-      if (content === "clear" || content === "cls") {
-        newLines = [];
-      } else {
-        newLines.push(`┌──(ak㉿ak)-[~]`);
-        newLines.push(`└─$ ${content}`);
-        
-        
-        if (content === "ls") {
-          newLines = [...newLines, "info.txt", "skills.txt", "projects.txt", "contact.txt"];
-        }
-
-        else if (content === "pwd") {
-          newLines = [...newLines, "/home/ak"];
-        }
-        else if (content === "id") {
-          newLines = [...newLines, "uid=1000(ak) gid=1000(ak) groups=1000(ak),4(adm),24(cdrom),27(sudo),126(sambashare)"];
-        }
-
-        else if (content === "whoami") {
-          newLines = [...newLines, "ak"];
-        }
-
-        else if (content === "uname -a") {
-          newLines = [...newLines, "Linux ak 5.4.0-80-generic #90-Ubuntu x86_64 GNU/Linux - Portfolio Terminal"];
-        }
-
-        else if (content === "uname") {
-          newLines = [...newLines, "Linux"];
-        }
-
-        else if (content === "cat info.txt") {
-          newLines = [...newLines, "Name: Sujan Sharma", "Location: On, Canada", "Occupation: Computer Programmer"];
-        }
-
-        else if (content === "cat skills.txt") {
-          newLines = [...newLines, "Languages: JavaScript, Python, Java, C++, C, Ruby", "Frameworks: Django, React, Node.js, Express, Flask, Ruby on Rails", "Databases: MySQL, PostgreSQL, MongoDB, SQLite", "Tools: Git, Docker, Jenkins, Travis CI, Circle CI, Heroku, AWS, GCP"];
-        }
-
-        else if (content === "cat projects.txt") {
-          newLines = [...newLines, "1. Portfolio Terminal", "2. QBX", "3. Dynamic YouTube title"];
-        }
-
-        else if (content === "cat contact.txt") {
-          newLines = [...newLines, "Email: sujan@brokenai.ca, sujansharma2060@gmail.com", "LinkedIn: https://www.linkedin.com/in/NotSujanSharma/", "GitHub: https://www.github.com/NotSujanSharma"];
-        }
-
-        else if (content === "help") {
-          newLines = [...newLines, "Available commands are: ls, cat, pwd, id, whoami, uname, clear, help"];
-        }
-          
-          else {
-            newLines = [...newLines, `command not found: ${content}`];
-          }
-        newLines.push(` `);
-      }
-      
-
-      setTerminalLines(newLines);
-      event.target.value = ''; 
+      executeCommand(event.target.value);
+      event.target.value = '';
     }
   };
 
@@ -94,7 +71,7 @@ function App() {
             <div className='maximizeButton round bg-green action-button'>             
             </div>
           </div>
-          <div className='title'>sujansharma2060/portfolio-terminal</div>
+          <div className='title'>NotSujanSharma/portfolio-terminal</div>
         </nav>
       </header>
       <main>
